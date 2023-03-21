a507aea98d189102c2b39eeb972acf550452e3af
mariagsmn <mariagassmann0@gmail.com>
2023-03-21 16:06:18 +0100

    random

---
+++ L01_Recap & Foundation/my-script.js
@@ -0,0 +1,7 @@
+let v = 1;
+v = v + 1;
+console.log(v);
+let v1 = 1;
+let v2 = 2;
+v1 = v2;
+//# sourceMappingURL=my-script.js.map
---
+++ L01_Recap & Foundation/my-script.ts
@@ -0,0 +1,8 @@
+let v: number = 1;
+v = v + 1;
+console.log(v);
+
+let v1: number = 1
+let v2: number = 2
+
+v1 = v2
