e973c0668ad98f8fdd46e83e2d6ca55070b936d5
mariagsmn <mariagassmann0@gmail.com>
2023-04-06 12:55:38 +0200

    A03_fast_fertig

--- A03_Aufgabenliste_Formular/A3-script.js
+++ A03_Aufgabenliste_Formular/A3-script.js
@@ -1,1 +1,22 @@
+var A03;
+(function (A03) {
+    window.addEventListener("load", handleLoad);
+    function handleLoad() {
+        let task = document.querySelector(".task");
+        task.addEventListener("change", handleTask);
+        let date = document.querySelector("#date");
+        date.addEventListener("change", handleDate);
+        let time = document.querySelector("#time");
+        date.addEventListener("change", handleTime);
+    }
+    function handleTask() {
+        console.log("Aufgabe wurde hinzugefügt");
+    }
+    function handleDate() {
+        console.log("Datum wurde geändert");
+    }
+    function handleTime() {
+        console.log("Zeit wurde geändert");
+    }
+})(A03 || (A03 = {}));
 //# sourceMappingURL=A3-script.js.map
